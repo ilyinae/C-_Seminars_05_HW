8e956cd71cee91d07bebf8849b89186cdcc09e4e
Anton Ilyin <ilyin.a.e@gmail.com>
2022-10-01 11:46:48 +0300

    Added Ex034

---
+++ Ex034/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+void fillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int releaseArr(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            counter++;
+    }
+    return counter;
+}
+
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+fillArray(array);
+Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine($"Количество четных чисел в массиве: {releaseArr(array)}");
